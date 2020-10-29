dcf39f7edd81ec17383a56e1c0dcff121d6755e8
Pandanom <stas110922@gmail.com>
2020-10-29 16:35:07 +0200

    last

--- TPR3/TPR3.cpp
+++ TPR3/TPR3.cpp
@@ -3,30 +3,74 @@
 
 #include <iostream>
 #include "Electre.h"
+using namespace std;
+struct TaskIn 
+{
+private:
+	void getNums()
+	{
+		cout << "Кiлькiсть альтернатив: ";
+		cin >> aNum;
+		cout << "Кiлькiсть критерiїв: ";
+		cin >> kNum;
+		cout << "Порогове значення iндексу згоди: ";
+		cin >> a0;
+		cout << "Порогове значення iндексу незгоди: ";
+		cin >> b0;
+	}
+
+	void getW()
+	{
+		w = new double[kNum];
+		cout << "Введiть ваги критерiїв w" << endl;
+		for (int i = 0; i < kNum; i++)
+			cin >> w[i];
+	}
+	void getD()
+	{
+		d = new double*[aNum];
+		for (int i = 0; i < aNum; i++)
+			d[i] = new double[kNum];
+		for (int i = 0; i < aNum; i++)
+		{
+			cout << endl;
+			cout << "A" << i << " ";
+			for (int j = 0; j < kNum; j++)
+				cin >> d[i][j];
+		}
+	
+	
+	}
+public:
+	double ** d;
+	double *w;
+	int kNum, aNum;
+	double a0, b0;
+
+
+
+
+	void GetValue()
+	{
+		getNums();
+		getW();
+		getD();
+	}
+};
+
+
+
 
 int main()
 {
-	double ** d;
-	d = new double*[4];
-	d[0] = new double[5]{ 9,8,1,3,7 };
-	d[1] = new double[5] {3,7,3,6,5,};
-	d[2] = new double[5] {1,2,5,1,3};
-	d[3] = new double[5] {2,5,1,7,5};
-	auto w = new double[5]{ 9,5,3,1,5 };
-	Electre e(5, 4, 0.8, 0.5);
-	e.SetMatrix(d, w);
+	system("chcp 1251");
+	
+	TaskIn t;
+	t.GetValue();
+	Electre e(t.kNum, t.aNum, t.a0, t.b0);
+	e.SetMatrix(t.d, t.w);
 	e.CalculateAB();
 	e.PrintAnswer();
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
